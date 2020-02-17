760160983cabe52aec6f46b73d16be894c9851f5
Julian Wyatt <julian.wyatt@durham.ac.uk>
2020-02-17 20:00:49 +0000

    servered more models

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/",express.static(path.join(__dirname, 'client')));
 app.use('/lib',express.static('client/lib')); 
-console.log(path.join(__dirname, 'client', 'lib'))
 
 app.get("/mug",function(req,res){
 	fs.readFile(path.join(__dirname, 'Models', 'mug.obj'),function(err,data){
@@ -28,5 +27,34 @@ app.get("/sofa1",function(req,res){
 		res.send(data);
 	})
 });
-
+app.get("/bulb",function(req,res){
+	fs.readFile(path.join(__dirname, 'Models', 'bulb.obj'),function(err,data){
+		res.send(data);
+	})
+});
+app.get("/plate",function(req,res){
+	fs.readFile(path.join(__dirname, 'Models', 'plate.obj'),function(err,data){
+		res.send(data);
+	})
+});
+app.get("/soundbar",function(req,res){
+	fs.readFile(path.join(__dirname, 'Models', 'soundbar.obj'),function(err,data){
+		res.send(data);
+	})
+});
+app.get("/sofa2",function(req,res){
+	fs.readFile(path.join(__dirname, 'Models', 'sofa2.obj'),function(err,data){
+		res.send(data);
+	})
+});
+app.get("/TV_Stand",function(req,res){
+	fs.readFile(path.join(__dirname, 'Models', 'TV_Stand.obj'),function(err,data){
+		res.send(data);
+	})
+});
+app.get("/TV",function(req,res){
+	fs.readFile(path.join(__dirname, 'Models', 'TV.obj'),function(err,data){
+		res.send(data);
+	})
+});
 app.listen(8000);
